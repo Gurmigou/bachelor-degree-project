4d4c43c6c5fbd6edd8d5a84fa1a1473fc1a19872
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-04-17 23:06:46 +0300

    Implemented star component for look card

---
+++ front-end/src/app/shared/star-rating/star-rating.component.ts
@@ -0,0 +1,39 @@
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrls: ['./star-rating.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class StarRatingComponent implements OnInit {
+
+  @Input('rating') rating: number = 3;
+  @Input('starCount') starCount: number = 5;
+  @Output() private ratingUpdated = new EventEmitter();
+
+  ratingArr: number[] = [];
+
+  constructor() {
+  }
+
+
+  ngOnInit() {
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
+  }
+
+  onClick(rating: number) {
+    this.ratingUpdated.emit(rating);
+    return false;
+  }
+
+  showIcon(index: number) {
+    if (this.rating >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
+}
