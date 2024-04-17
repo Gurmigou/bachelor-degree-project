d5dc85206fd3222ba568ecb23c8ddcc445dca91b
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-04-17 23:08:06 +0300

    Updated Routes

--- front-end/src/app/routes/Routes.ts
+++ front-end/src/app/routes/Routes.ts
@@ -2,9 +2,11 @@ import {Routes} from "@angular/router";
 import {LoginComponent} from "../pages/login/login.component";
 import {RegistrationComponent} from "../pages/registration/registration.component";
 import {NotFoundComponent} from "../pages/not-found/not-found.component";
+import {MarketComponent} from "../pages/market/market/market.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: RegistrationComponent},
+  {path: 'market', component: MarketComponent},
   {path: '**', component: NotFoundComponent},
 ];
