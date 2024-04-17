b1f52ec03ac885923916a5aafa61d0ab07cf1544
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-04-17 23:09:13 +0300

    Implemented look card component

---
+++ front-end/src/app/shared/look-card/look-card.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-look-card',
+  templateUrl: './look-card.component.html',
+  styleUrl: './look-card.component.css'
+})
+export class LookCardComponent {
+
+}
