58487a74515e3497bd3a652207e5298004961a5f
YehorBukhane <yehor.bukhanets@software-aspekte.de>
2024-04-19 00:11:03 +0300

    Implemented Tag component

---
+++ front-end/src/app/shared/tag/tag.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.css'
+})
+export class TagComponent {
+  @Input()
+  label: string = ""
+
+  constructor() {
+  }
+}
